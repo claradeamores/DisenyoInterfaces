06ed492556a62679bc4cf60228203533218fa3be
claradeamores <claradeamores@gmail.com>
2024-01-31 17:54:02 +0100

    no message

--- segundoParcial/act1Jquery/js/common.js
+++ segundoParcial/act1Jquery/js/common.js
@@ -1,4 +1,33 @@
 $(document).ready(function () {
-    // Habilitar el botón de la clase izquierda al cargar la página
-    $('.izquierda .btn-arriba').prop('disabled', false).css('cursor', 'pointer');
+    $('#btn-arriba').click(function () {
+        //$('#acceso').hide();
+        // $('#container-cuenta').show();
+        $('#container-cuenta').slideDown();
+        $('#acceso').hide();
+
+    })
+    // Agregar manejadores de eventos keyup para los campos de entrada 
+    $('#correo, #contrasenya').on('keyup', function () {
+        // Verificar si ambos campos de entrada tienen valores
+        if ($('#correo').val() !== '' && $('#contrasenya').val() !== '') {
+            // Habilitar el botón "Iniciar sesión"
+            $('#iniciar-sesion').prop('disabled', false);
+        } else {
+            // Deshabilitar el botón "Iniciar sesión" si algún campo está vacío
+            $('#iniciar-sesion').prop('disabled', true);
+        }
+    });
+    $(document).ready(function () {
+        $('#btn-abajo').click(function () {
+            $('#acceso').fadeOut(function () {
+                // Esta función de callback se ejecuta después de completar el desvanecimiento
+                $('#container-usuario').fadeIn();
+            });
+        });
+    });
+
+
 });
+
+
+
